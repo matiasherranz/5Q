603fbdb5ba3c400229c857235e38adada7be761f
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-03-06 17:18:12 -0300

    Word counting urls.

--- code/FiveQTest/devotional/urls.py
+++ code/FiveQTest/devotional/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('devotional',
     url(r'^view_devotionals/', 'views.view_devotionals',
             name='view_devotionals'),
+    url(r'^view_words_count/', 'views.render_words_count',
+            name='view_words_count'),
 )
