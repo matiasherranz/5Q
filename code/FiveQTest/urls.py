5d68c476d363bcd10c1d0c000d64d06b3d2e807c
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-03-06 17:02:57 -0300

    Urls work.

--- code/FiveQTest/urls.py
+++ code/FiveQTest/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 *-*
-from django.conf import settings
+#from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 
 # Enabling the Django admin interface:
@@ -9,24 +9,16 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    # Project urls:
-    url(r'^common', include('common.urls')),
-
-    url(r'^', 'django.views.generic.simple.redirect_to',
-        {'url': '/common/dashboard/'}),
-    url(r'^/', 'django.views.generic.simple.redirect_to',
-        {'url': '/common/dashboard/'}),
-
-
     # Django admin:
     url(r'^admin/', include(admin.site.urls)),
 
     # User authentication:
     url(r'^accounts/', include('registration.backends.default.urls')),
-)
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^site-media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    )
+    # Project urls:
+    url(r'^$', 'django.views.generic.simple.redirect_to',
+        {'url': '/common/dashboard/'}),
+
+    url(r'^common/', include('common.urls')),
+    url(r'^devotional/', include('devotional.urls')),
+)
