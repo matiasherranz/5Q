8350eff61df63f32c7c5fc2e76db193f248b4b5e
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-03-06 15:42:45 -0300

    Added a view for the dashboard.

--- code/FiveQTest/common/views.py
+++ code/FiveQTest/common/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from django.core.urlresolvers import reverse
+
+
+@login_required
+def render_dashboard(request):
+    """
+    Render project dashboard.
+    """
+    links = {'Django admin': reverse('admin:index'),
+             'Logout': reverse('auth_logout'), }
+
+    return render_to_response('common/dashboard.html',
+                              {'links': links},
+                              context_instance=RequestContext(request))
