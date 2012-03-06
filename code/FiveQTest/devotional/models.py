e487f4ab23527928e714c3be34a3d927cf5ed2a7
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-03-06 17:01:44 -0300

    Model work for the devotionals.

--- code/FiveQTest/devotional/models.py
+++ code/FiveQTest/devotional/models.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Devotional(models.Model):
+    date = models.DateField("Date of the devotional.")
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=1000, null=True, blank=True)
+    tags = TaggableManager()
+
+    def __unicode__(self):
+        return "%s - %s" % (self.title, self.date)
